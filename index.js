91b1edeb7f3ee5335272f17f1b946622663fa422
paulamartineza <paula.macero2002@gmail.com>
2025-06-12 11:03:51 -0500

    Disparo el workflow CI/CD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola mundo DevvOps!');
+  res.send('¡Hola mundo DevOps!');
 });
 
 app.get('/inicio', (req, res) => {
